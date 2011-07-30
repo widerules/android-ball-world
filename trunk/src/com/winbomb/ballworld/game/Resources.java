5cb2a7e009628bc336c56b4a9afaf703c0fbb0b7
lee.winbomb <lee.winbomb@b2cd5ac1-8a4e-d5f4-afda-422a170abe22>
2011-07-30 04:05:09 +0000

    洞使用图像，而不是直接画。

--- trunk/src/com/winbomb/ballworld/game/Resources.java
+++ trunk/src/com/winbomb/ballworld/game/Resources.java
@@ -10,12 +10,14 @@ public class Resources {
 	private static Resources instance = null;
 	public static Bitmap imgBall;
 	public static Bitmap imgBg;
+	public static Bitmap imgHole;
 
 	private Resources(Context ctx) {
 		context = ctx;
 
 		imgBall = loadBitmap(R.drawable.img_ball);
 		imgBg = loadBitmap(R.drawable.img_bg);
+		imgHole = loadBitmap(R.drawable.hole);
 	}
 
 	public synchronized static void initResources(Context context) {
