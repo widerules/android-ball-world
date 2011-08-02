a75670dec3c43b8ef370f06b729f28bf998ee682
lee.winbomb <lee.winbomb@b2cd5ac1-8a4e-d5f4-afda-422a170abe22>
2011-08-02 11:12:28 +0000

    修改了BallWorld的默认宽度。

--- src/com/winbomb/ballworld/Setting.java
+++ src/com/winbomb/ballworld/Setting.java
@@ -15,13 +15,13 @@ public interface Setting {
 	public static final float VELOCITY_THREADHOLD = -200.0f;
 
 	/** 从孔中能够逃离的速度阈值 */
-	public static final float ESCAPE_VELOCITY = 50f;
+	public static final float ESCAPE_VELOCITY = 100f;
 
 	/** Ball World的默认宽度 */
-	public static final int WORLD_WIDTH = 320;
+	public static final int WORLD_WIDTH = 300;
 
 	/** Ball World的默认高度 */
-	public static final int WORLD_HEIGHT = 480;
+	public static final int WORLD_HEIGHT = 440;
 
 	/** 从重力感应获取的数值与BallWorld使用的gravity之间的比值 */
 	public static final float GRAVITY_RATIO = 98 * 2;
